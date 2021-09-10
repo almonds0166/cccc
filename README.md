9992df2619b7a3be7606efbc31f5bd63023a9eeb
Madison Landry <mlandry@mit.edu>
2021-09-10 11:00:49 -0400

    Fall 2021 update
    * Update README
    * Refactor code
    * Add inherent support for Google Chrome
    * Improve command-line interface
    * Add .gitignore
    * Add requirements.txt

--- README.md
+++ README.md
@@ -2,28 +2,20 @@
 
 Claims first unclaimed student in a CAT-SOOP Queue.
 
-At MIT, some subjects running [CAT-SOOP](https://catsoop.mit.edu/website) sites have "queues" where students ask for help or for "checkoffs." You'll be interested in this simple script if you're a lab assistant or TA or plan to be one for such a class and you know what I'm referring to.
+At MIT, some subjects running [CAT-SOOP](https://catsoop.mit.edu/website) sites have *help queues* where students ask for help or for "checkoffs." You'll be interested in this simple script if you're a lab assistant or TA or plan to be one for such a class and you know what at all I'm referring to.
 
-### How it works:
+Currently, the script is only for Windows OS (due to the `pwin32` module), and it looks for the help queue on either Firefox or Google Chrome (read script for how it does this).
 
-![Empty Queue](https://i.imgur.com/1mGSGSO.png)
+Use with discretion. This gives you an arguably unfair advantage if you and the people you work with are the kind to enjoy your job as a LA/TA and helping the students in general.
 
-* Have the Queue open in your browser like above
-* Run `cccc.py`
-* If the script sees that the browser showing the Queue is the active window, it will wait for a student to add theirself
-* At that point, the script presses `c` on your keyboard for you until the student is claimed
-* `Ctrl+C` to quit
+### Starting up
 
-Use with discretion. This gives you an arguably unfair advantage if you and the people you work with are the kind to enjoy giving checkoffs and helping the students in general.
+1. Install the dependencies (`pywin32`, `keyboard`)
+2. Have the Queue open in your browser like this:
 
-### Assumes:
+![Empty Queue](https://i.imgur.com/eM6dEvB.jpg)
 
-* Windows OS
-* Firefox browser
-
-The latter assumption should be straightforward to change if desired.
-
-### Dependencies:
-
-* `pywin32`
-* `keyboard`
+3. Run `cccc.py`
+4. If the script sees that the browser showing the Queue is the active window, it will wait for students to add themselves
+5. When there's an unclaimed student, the script presses `c` on your keyboard for you until the student is claimed
+6. `Ctrl+C` to quit
--- cccc.py
+++ cccc.py
@@ -1,25 +1,69 @@
-import keyboard
+
+import re
 from time import time, sleep
+from sys import stdout
+
+import keyboard
 from win32gui import GetWindowText, GetForegroundWindow, SetForegroundWindow
 
-FPS = 20                         # rate
-browser_name = "Mozilla Firefox" # "Google Chrome", etc.
+BROWSER_NAMES = ("Mozilla Firefox", "Google Chrome")
 
-timeout = 1 / FPS
-timer = time()
-try:
+# rate
+FPS = 10
+TIMEOUT = 1 / FPS
+
+# broswer tab title regex, e.g. "(0/0) Queue"
+QUEUE = re.compile(r"\(([0-9]+)\/([0-9]+)\) Queue")
+
+def main():
+   timer = time()
+   max_line_len = 0
+
+   # system's intial states:
+   # (True means the active window is the CAT-SOOP help queue)
+   last_state = True
+   current_state = False
+
    while True:
       active_title = GetWindowText(GetForegroundWindow())
 
-      if browser_name in active_title:
+      current_state = False
+      if any(name in active_title for name in BROWSER_NAMES):
 
-         unclaimed = active_title.split("/", 1)[0][1:]
-         if unclaimed.isdigit() and int(unclaimed):
-            keyboard.press_and_release('c')
-            #print("c", end="")
+         m = QUEUE.match(active_title)
 
-      sleep(max(0, timeout - (time() - timer)))
+         if m and m.group(1).isdigit() and m.group(2).isdigit():
+            unclaimed = int(m.group(1))
+            claimed = int(m.group(2))
+            # at this point, we're pretty confident that the detected window is
+            # the help queue
+            current_state = True
+
+            if unclaimed:
+               keyboard.press_and_release('c')
+
+      if current_state != last_state:
+         stdout.write("\r")
+         if not current_state:
+            line = "(Active window is not the help queue)"
+         else:
+            line = f"Claimed: {claimed:<2d} Unclaimed: {unclaimed:<2d}"
+            if unclaimed:
+               line += " c!"
+         
+         max_line_len = max(max_line_len, len(line))
+
+         stdout.write(" " * max_line_len + "\r")
+         stdout.write(line)
+         stdout.flush()
+
+      last_state = current_state
+
+      sleep(max(0, TIMEOUT - (time() - timer)))
       timer = time()
 
-except KeyboardInterrupt:
-   print("^C")
+if __name__ == "__main__":
+   try:
+      main()
+   except KeyboardInterrupt:
+      print("\n^C")
